59bc1a29c8b6752bccd21592487e9ac30cab72a5
Laura Wolfart <laurawolfart@gmail.com>
2017-08-09 19:50:29 -0400

    forms placeholder are loading

--- client/src/components/Lady.js
+++ client/src/components/Lady.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import '../App.css';
 
 class Lady extends Component {
+
+  renderField(field) {
+    return (
+      <div>
+        <label htmlFor="field">{field.label}</label>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
           <h2>Orlando Lady Devs</h2>
+          <p className="App-intro">
+            We want to get to know you bettter!
+          </p>
         </div>
-        <p className="App-intro">
-          Who are the lady devs?
-        </p>
+
+        <form>
+          <Field
+            label="First Name"
+            name="firstName"
+            component={this.renderField}
+          />
+          <Field
+            label="Last Name"
+            name="lastName"
+            component={this.renderField}
+          />
+          <Field
+            label="Email"
+            name="email"
+            component={this.renderField}
+          />
+
+        </form>
 
       </div>
     );
   }
 }
 
-export default Lady;
+export default reduxForm({
+  form: 'LadiesNewForm'
+})(Lady);
