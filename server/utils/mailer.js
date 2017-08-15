a26de1843c25019ec33a04dc6e8b68203ce17db6
Aaron Gottlieb <atticus19@gmail.com>
2017-08-15 10:05:44 -0600

    adds email templates to sendMail function

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const nodemailer = require('nodemailer');
+const mjmlUtils = require('mjml-utils');
 
 const options = {
   host: 'smtp.gmail.com',
@@ -10,24 +12,31 @@ const options = {
   }
 };
 
+//create transporter
 const smtpTransport = nodemailer.createTransport(options);
 
-const sendMail = (recipient, subject) =>
-  new Promise((resolve, reject) => {
-    const mailOptions = {
-      from: 'Lady Devs <dev.forms.node@gmail.com>',
-      to: recipient,
-      subject,
-      text: 'We have received your organizer request and approve it pronto',
-      html: '<b>Lady Devs Rule!!<b>'
-    };
-    smtpTransport.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(info);
-      }
-    });
-  });
+const sendMail = (recipient, subject) => {
+  const welcomeEmailTemplate = path.join(__dirname, '../email-templates/html/welcome.html');
+  return mjmlUtils.inject(welcomeEmailTemplate, { name: 'Aaron' })
+  .then(finalTemplate =>
+      new Promise((resolve, reject) => {
+        const mailOptions = {
+          from: 'Lady Devs <dev.forms.node@gmail.com>',
+          to: recipient,
+          subject,
+          text: 'We have received your organizer request and approve it pronto',
+          html: finalTemplate
+        };
+        smtpTransport.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(info);
+          }
+        });
+      })
+  )
+  .catch(error => console.log('send mail function error', error));
+};
 
 module.exports = sendMail;
